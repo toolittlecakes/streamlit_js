f4aeeba36c0e2cc4670afa37112531b3ca950ef3
sne <nickolay.sheyko@gmail.com>
2024-03-21 20:34:49 +0700

    github url in setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     description="Streamlit component that allows you to run js code with both blocking and non-blocking modes.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/toolittlecakes/streamlit_js",
     packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=[],
